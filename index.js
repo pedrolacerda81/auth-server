1b4bdbe06fc787cf00bf906fe67f1b9a9bcc65e7
Pedro - GitPro <peidom81@gmail.com>
2020-06-12 00:55:46 -0400

    Creates migrations, middleware and routes

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const app = require('express')()
+const consign = require('consign')
+const db = require('./config/db')
+
+app.db = db
+
+consign()
+    .then('./config/middleware.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
 
 app.listen(4000, () => {
     console.log('Auth server executando...')
